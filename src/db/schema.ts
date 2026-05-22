ee64ff1e6240a7b9d94e510321d4fb95f7de22a9
Dimitar <dimitar.mandarliev@gmail.com>
2026-05-22 23:19:13 +0300

    Setting up PostgreSQL database and creating table schemas with Drizzle

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,20 @@
-import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core'
+import { date, integer, numeric, pgTable, text } from 'drizzle-orm/pg-core'
 
-export const todos = pgTable('todos', {
-  id: serial().primaryKey(),
-  title: text().notNull(),
-  createdAt: timestamp('created_at').defaultNow(),
+export const categoriesTable = pgTable('categories', {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: text().notNull(),
+  type: text({
+    enum: ['income', 'expense'],
+  }).notNull(),
+})
+
+export const transactions = pgTable('transactions', {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  userId: text('user_id').notNull(),
+  description: text().notNull(),
+  amount: numeric().notNull(),
+  transactionDate: date('transaction_date').notNull(),
+  categoryId: integer('category_id')
+    .references(() => categoriesTable.id)
+    .notNull(),
 })
