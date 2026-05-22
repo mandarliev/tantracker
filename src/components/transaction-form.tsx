5a687ff9114c6f6a6230276311000d9d36d8c330
Dimitar <dimitar.mandarliev@gmail.com>
2026-05-22 17:03:16 +0300

    Transaction form schema using zod

--- src/components/transaction-form.tsx
+++ src/components/transaction-form.tsx
@@ -1,3 +1,18 @@
+import * as z from 'zod'
+
+const transactionFormSchema = z.object({
+  transactionType: z.enum(['income', 'expense']),
+  categoryId: z.coerce.number().positive('Please select a category'),
+  transactionDate: z
+    .date()
+    .max(new Date(), 'Transaction date cannot be in the future'),
+  amount: z.coerce.number().positive('Amount must be greater than 0'),
+  description: z
+    .string()
+    .min(3, 'Description must contain at least 3 charachters')
+    .max(300, 'Description must contain a maximum of 300 characters'),
+})
+
 export function TransactionForm() {
   return <div>Transaction Form</div>
 }
