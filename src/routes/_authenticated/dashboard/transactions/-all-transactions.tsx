c343b6da065843d4eb8e9e8eaa9cb9b92eaf1451
Dimitar <dimitar.mandarliev@gmail.com>
2026-05-27 14:31:30 +0300

    Implementing month and year selection

--- src/routes/_authenticated/dashboard/transactions/-all-transactions.tsx
+++ src/routes/_authenticated/dashboard/transactions/-all-transactions.tsx
@@ -1,5 +1,15 @@
+import { Button } from '#/components/ui/button'
 import { Card, CardHeader, CardTitle } from '#/components/ui/card'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '#/components/ui/select'
+import { Link } from '@tanstack/react-router'
 import { format } from 'date-fns'
+import { useState } from 'react'
 
 export function AllTransactions({
   month,
@@ -8,11 +18,50 @@ export function AllTransactions({
   month: number
   year: number
 }) {
+  const [selectedMonth, setSelectedMonth] = useState(month)
+  const [selectedYear, setSelectedYear] = useState(year)
+
   const selectedDate = new Date(year, month - 1, 1)
   return (
     <Card className="mt-4">
       <CardHeader>
-        <CardTitle>{format(selectedDate, 'MMM yyyy')} Transactions</CardTitle>
+        <CardTitle className="flex justify-between">
+          <span>{format(selectedDate, 'MMM yyyy')} Transactions</span>
+          <div className="flex gap-1">
+            <Select
+              value={selectedMonth.toString()}
+              onValueChange={(value) => setSelectedMonth(Number(value))}
+            >
+              <SelectTrigger>
+                <SelectValue />
+              </SelectTrigger>
+              <SelectContent>
+                {Array.from({ length: 12 }).map((_, i) => (
+                  <SelectItem key={i} value={`${i + 1}`}>
+                    {format(new Date(selectedDate.getFullYear(), i, 1), 'MMM')}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+            <Select
+              value={selectedYear.toString()}
+              onValueChange={(value) => setSelectedYear(Number(value))}
+            >
+              <SelectTrigger>
+                <SelectValue />
+              </SelectTrigger>
+              <SelectContent></SelectContent>
+            </Select>
+            <Button asChild>
+              <Link
+                to="/dashboard/transactions"
+                search={{ month: selectedMonth, year: 2024 }}
+              >
+                Go
+              </Link>
+            </Button>
+          </div>
+        </CardTitle>
       </CardHeader>
     </Card>
   )
